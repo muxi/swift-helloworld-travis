4a94d4120bc458a1e0f8c21095c70dc4c41bc220
Muxi Yan <muxi@users.noreply.github.com>
2017-02-18 18:46:21 -0800

    Bye wolrd

--- sources/main.swift
+++ sources/main.swift
@@ -3,7 +3,7 @@ import Glibc
 
 let player = ["rock", "paper", "scissors", "lizard", "spock"]
 
-print("Hello World!")
+print("Bye World!")
 print("Let's play rock, paper, scissors, lizard, spock!")
 
 srandom(UInt32(NSDate().timeIntervalSince1970))
